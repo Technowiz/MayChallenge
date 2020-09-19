d055dd2335338fbf01022dc4e9ef8928ccc9bf8e
Saranya S <saranyadec6@gmail.com>
2020-09-19 13:22:23 +0530

    Web API with MVC UI

---
+++ WepAPIwithMVCUI/MVCWithWebAPITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MVCWithWebAPITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCWithWebAPITest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f790303d-7852-4768-889f-71ed89f9c245")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WepAPIwithMVCUI/MVCWithWebAPITest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCwithWebAPI.Controllers;
+using MVCwithWebAPI.Models;
+
+namespace MVCWithWebAPITest
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        Employee emp1 = null;
+        Employee emp2 = null;
+        List<Employee> emps = null;
+        EmployeesController controller = null;
+
+        public EmployeeControllerTest()
+        {
+            // Lets create some sample books
+            emp1 = new Employee { Name = "Tony", Address = "Chennai", Gender = "Female", Company = "NA", Designation = "NA" };
+            emp2 = new Employee { Name = "John", Address = "NA", Gender = "Male", Company = "NA", Designation = "NA" };
+
+            emps = new List<Employee>
+            {
+                emp1,
+                emp2,
+            };
+            controller = new EmployeesController();
+        }
+        [TestMethod]
+        public void Create()
+        {
+            // Lets create a valid book objct to add into
+            Employee newEmp = new Employee { Name = "new", Address = "new", Gender = "NA" ,Company="NA",Designation="NA"};
+
+            // Lets call the action method now
+            controller.Create(newEmp);
+
+            // get the list of books
+            List<Employee> emps = 
+
+            CollectionAssert.Contains(emps, newEmp);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            // Lets create a valid book objct to add into
+            Book editedBook = new Book { ID = 1, BookName = "new", AuthorName = "new", ISBN = "NA" };
+
+            // Lets call the action method now
+            controller.Edit(editedBook);
+
+            // get the list of books
+            List<Book> books = booksRepo.GetAllBooks();
+
+            CollectionAssert.Contains(books, editedBook);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Lets call the action method now
+            controller.Delete(1);
+
+            // get the list of books
+            List<Book> books = booksRepo.GetAllBooks();
+
+            CollectionAssert.DoesNotContain(books, book1);
+        }
+    }
+}
